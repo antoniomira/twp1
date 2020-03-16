4ef6cf3b9c0ed607b26b1e832570d0939fc14ce9
antoniomira <antonio.camacho@goumh.umh.es>
2020-03-16 16:15:31 +0100

    Conflictos solucionados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AppRoutingModule } from './app-routing.module';
-=======
 import { IndexComponent } from './components/index/index.component';
->>>>>>> feature1
 
 @NgModule({
   declarations: [
