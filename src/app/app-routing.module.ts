1eb02f810a1f02afb5964c1cca4cacf0d0d394a9
antoniomira <antonio.camacho@goumh.umh.es>
2020-03-16 16:23:33 +0100

    El router y la feature1 funcionan correctamente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // Importar los componentes a los que se quiere navegar.
+import { IndexComponent } from "../app/components/index/index.component";
 
 const routes: Routes = [
   //{ path: 'heroes', component: HeroesComponent }
+  { path: '', component: IndexComponent }
 ];
 
 @NgModule({
